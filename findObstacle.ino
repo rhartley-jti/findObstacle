7aa714cb9efc71370520c3561a980ee687f260dc
Ryan Hartley <ryan.hartley@gmail.com>
2015-04-02 12:15:51 -0600

    Work we did in training

--- findObstacle.ino
+++ findObstacle.ino
@@ -24,7 +24,7 @@ struct ZumoState{
 
 void setup() {
   Serial.begin(9600);
-  Wire.begin();
+  Wire.begin(); //compass and gyro
   
   compass.init();
   compass.enableDefault();
@@ -39,9 +39,92 @@ void setup() {
   Serial.println("Ready to calibrate?");
 
   buzzer.play(">g32");
-  button.waitForButton();
+  //button.waitForButton();
   delay(500);
-  DoCalibrate();
+  //button.waitForButton();
+  //DoCalibrate();
+  button.waitForButton();
+  Serial.println("Ready for action?");
+  buzzer.play(">g32>>c32");
+  delay(500);
+}
+
+void SetMotorsReadSensors(struct ZumoState* s){
+  gyro.read();
+  compass.read();
+  s->ax = compass.a.x;
+  s->ay = compass.a.y;
+  s->az = compass.a.z;
+  s->wx = gyro.g.x;
+  s->wy = gyro.g.y;
+  s->wz = gyro.g.z;
+  
+  /*
+  snprintf(report, sizeof(report), "%4d,%4d, %6d ax,%6d ay,%6d az, %6d wx,%6d wy,%6d wz",
+    s->left, s->right,
+    s->ax, s->ay, s->az,
+    s->wx, s->wy, s->wz);
+    */
+    snprintf(report, sizeof(report), "%6d ax,%6d ay",
+    s->ax, s->ay);
+  Serial.println(report);
+}
+
+char ObstacleDetected(struct ZumoState* s){
+  //acceleration
+  if (-14500 > s->ax || s->ax > 6500){
+    if (s->ax > 0){
+      return 0;
+    }
+    else
+      return 1;
+  }
+  if (-2800 > s->ay || s->ay > 1900){
+    if (s->ay > 0){
+      return 2;
+    }
+    return 3;
+  }
+  if (12600 > s->az || s->az > 24600){
+    //return true;
+  }
+  return -1;
+}
+
+void loop() {
+  struct ZumoState s;  
+  
+  
+  SetMotorsReadSensors(&s);
+  delay(500);
+  motors.setSpeeds(200, 200);
+  delay(500);
+  char d = -1;
+  do{
+    SetMotorsReadSensors(&s);
+    delay(20);
+    d = ObstacleDetected(&s);
+  } while (d == -1);
+  if (d == 0)
+  {
+    motors.setSpeeds(-400, 400);
+   delay(200); 
+  }
+  if (d == 1)
+  {
+   motors.setSpeeds(-200, -200);
+  delay(100); 
+  }
+  if (d == 3)
+  {
+   motors.setSpeeds(-100, 100);
+  delay(50); 
+  }
+  if (d == 2)
+  {
+   motors.setSpeeds(100, -100);
+  delay(50); 
+  }
 }
 
 void DoCalibrate(){
@@ -54,7 +137,7 @@ void DoCalibrate(){
   }
   s.left = 200;
   s.right = 200;
-  for (int i = 0; i < 50; i++){
+  for (int i = 0; i < 20; i++){
       SetMotorsReadSensors(&s);
       delay(20);
   }
@@ -72,41 +155,4 @@ void DoCalibrate(){
       delay(20);
   }
   motors.setSpeeds(0,0);
-}
-
-void SetMotorsReadSensors(struct ZumoState* s){
-  gyro.read();
-  compass.read();
-  motors.setSpeeds(s->left, s->right);
-  s->ax = compass.a.x;
-  s->ay = compass.a.y;
-  s->az = compass.a.z;
-  s->wx = gyro.g.x;
-  s->wy = gyro.g.y;
-  s->wz = gyro.g.z;
-  
-  snprintf(report, sizeof(report), "%4d,%4d, %6d,%6d,%6d, %6d,%6d,%6d",
-    s->left, s->right,
-    s->ax, s->ay, s->az,
-    s->wx, s->wy, s->wz);
-  Serial.println(report);
-}
-
-bool ObstacleDetected(struct ZumoState* s){
-  return false;
-}
-
-void loop() {
-  struct ZumoState s;
-  s.left = 200;
-  s.right = 150;
-  Serial.println("Ready for action?");
-  buzzer.play(">g32>>c32");
-  button.waitForButton();
-  delay(1000);
-  do{
-    SetMotorsReadSensors(&s);
-    
-    delay(20);
-  } while ( !ObstacleDetected(&s));
 }
